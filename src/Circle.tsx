dab34cf11d8fb8c9ddf5e141c8a76311cc9977b0
seongyeon <ysy2565@naver.com>
2022-05-22 21:40:36 +0900

    feat: add ts state

--- src/Circle.tsx
+++ src/Circle.tsx
@@ -1,3 +1,4 @@
+import { useState } from "react";
 import styled from "styled-components";
 
 interface ContainerProps {
@@ -19,6 +20,10 @@ interface CircleProps {
 
 function Circle({ bgColor, borderColor }: CircleProps) {
   // bgColor의 타입은 CircleProps의 Object이다!
+  //타입스크립트의 초기값을 가지고 counter이 number 타입이라고 추측!
+  const [value, setValue] = useState<number | string>(0);
+  setValue("hello");
+  setValue(2);
   return <Container bgColor={bgColor} borderColor={borderColor ?? bgColor} />;
 }
 //위와 같은 코드임!
