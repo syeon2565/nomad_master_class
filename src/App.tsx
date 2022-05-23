dae1c1ef0ca7cc1db5fdb5319cf8b22374b772d9
seongyeon <ysy2565@naver.com>
2022-05-23 12:08:51 +0900

    ts form value

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,28 @@
-import styled from "styled-components";
-import Circle from "./Circle";
+import { useState } from "react";
 
 function App() {
+  const [userName, setValue] = useState("");
+  const onChange = (event: React.FormEvent<HTMLInputElement>) => {
+    const {
+      currentTarget: { value },
+      // currentTarget은 EVEBTTarget & 형식에 HTMLInputElement 형식에서 value값을 찾아야한다!
+    } = event;
+    setValue(value);
+  };
+  const onSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+  };
   return (
     <>
-      <Circle borderColor="yellow" bgColor="teal"></Circle>
-      <Circle bgColor="tomato" />
+      <form onSubmit={onSubmit}>
+        <input
+          value={userName}
+          onChange={onChange}
+          type="text"
+          placeholder="username"
+        />
+        <button>Login</button>
+      </form>
     </>
   );
 }
